f8bc2c3ad61571ff7040b7534e1c11a26da48976
Ronald Wong <ronaldchunyin@gmail.com>
2016-06-06 03:05:35 +0800

    init

---
+++ demo_v1/www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('app.controllers', [])
+  
+.controller('page2Ctrl', function($scope) {
+
+})
+   
+.controller('page5Ctrl', function($scope) {
+
+})
+   
+.controller('page6Ctrl', function($scope) {
+
+})
+      
+.controller('page7Ctrl', function($scope) {
+
+})
+   
+.controller('SkyLighterCtrl', function($scope) {
+
+})
+   
+.controller('page9Ctrl', function($scope) {
+
+})
+   
+.controller('page10Ctrl', function($scope) {
+
+})
+   
+.controller('page11Ctrl', function($scope) {
+
+})
+   
+.controller('page12Ctrl', function($scope) {
+
+})
+   
+.controller('page13Ctrl', function($scope) {
+
+})
+   
+.controller('page14Ctrl', function($scope) {
+
+})
+   
+.controller('page15Ctrl', function($scope) {
+
+})
+ 
